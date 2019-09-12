efe1c14c17a65f69b6e2e84d2bbe79ab5aa55bb3
DaiQing <dq_199510@163.com>
2019-09-12 23:54:27 +0800

    learn jdk8 new things

---
+++ src/jdk8/ParallelArrays.java
@@ -0,0 +1,18 @@
+package jdk8;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ParallelArrays{
+	public static void main(String[] args){
+		long[] arrayOfLong = new long[2000];
+		Arrays.parallelSetAll(arrayOfLong,
+				index -> ThreadLocalRandom.current().nextInt(1000_000));
+		Arrays.stream(arrayOfLong).limit(10).forEach(i -> System.out.print(i + " "));
+		System.out.println();
+
+		Arrays.parallelSort(arrayOfLong);
+		Arrays.stream(arrayOfLong).limit(10).forEach(i -> System.out.print(i + " "));
+		System.out.println();
+	}
+}
